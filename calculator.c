995bd66595b20b7687f2ff8888c8e99297d0d006
niharika2024-gi <niharika.j@s.amity.edu>
2025-05-04 20:33:19 +0530

    Added subtraction function

--- calculator.c
+++ calculator.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
-    printf("Hello, Git!
-");
+    printf("Subtraction: %d\n", subtract(8, 3));
     return 0;
 }
